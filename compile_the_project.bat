c148494d298abd9b24ab7a774b870aab129fb9dd
jiang28555@gmail.com <jiang28555@gmail.com>
2018-06-29 17:05:17 +0800

    ch

--- compile_the_project.bat
+++ compile_the_project.bat
@@ -1,4 +1,4 @@
-@REM set JAVA_HOME=D:/usr/java/jdk1.8.0_65
-set JAVA_HOME=D:/usr/java/jdk1.7.0_51
+set JAVA_HOME=D:/usr/java/jdk1.8.0_65
+@REM set JAVA_HOME=D:/usr/java/jdk1.7.0_51
 call ant -f buildWin.xml
 pause
